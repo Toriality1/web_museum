6a98c1fdb3ab4913f5a9df9448c38ecbf177f4dc
Pedro Crelier <pcrelier@hotmail.com>
2025-07-18 06:15:25 -0300

    .

---
+++ 2000/index.php
@@ -0,0 +1,13 @@
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>2000</title>
+</head>
+
+<body>
+    <h1>Hello world!</h1>
+    <p>Current time is <?php echo date('H:i:s'); ?>.</p>
+</body>
+
+</html>
